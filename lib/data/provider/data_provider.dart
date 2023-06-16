43855528d5b58fb251853ef169e4884716fa9a0b
RomjanHossain <romjanhossain726526@gmail.com>
2023-06-16 19:56:03 +0600

    data layer

---
+++ lib/data/provider/data_provider.dart
@@ -0,0 +1,13 @@
+// all the data providers are defined here
+
+import 'package:gh/data/repo/profile_fetch_repo.dart';
+import 'package:gh/models/github_profile_mode.dart';
+import 'package:gh/utils/status_.dart';
+
+class DatabaseProvider {
+  // get the profile repo
+  Future<Status<GithubProfileModel, Exception>> getGithubProfile(
+      String username) {
+    return GetGithubProfileRepo(username).fetchProfile();
+  }
+}
