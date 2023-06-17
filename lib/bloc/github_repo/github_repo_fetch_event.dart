38a38bd4364081613923b3f6c028823aeda433cb
RomjanHossain <romjan@skiff.com>
2023-06-17 11:17:24 +0600

    github repo fetch completed

--- lib/bloc/github_repo/github_repo_fetch_event.dart
+++ lib/bloc/github_repo/github_repo_fetch_event.dart
@@ -8,3 +8,14 @@ class GithubRepoSearchEvent extends GithubRepoFetchEvent {
   final String username;
   GithubRepoSearchEvent(this.username);
 }
+
+// TODO: implement GithubRepoFetch paginated
+class GithubRepoFetchPaginated extends GithubRepoFetchEvent {
+  final String username;
+  final int pageNumber;
+  GithubRepoFetchPaginated(this.username, this.pageNumber);
+}
+
+class TestEvent extends GithubRepoFetchEvent {
+  TestEvent();
+}
