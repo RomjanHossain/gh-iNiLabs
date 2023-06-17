0796ae6b91edf600afbaa8bf90c1967b1c0ff84e
RomjanHossain <romjanhossain726526@gmail.com>
2023-06-17 06:10:14 +0600

    theme mode and consumer

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gh/bloc/dark_mode/cubit/theme_mode_cubit.dart';
 import 'package:gh/bloc/github_fetch/github_profile_bloc.dart';
 import 'package:gh/screens/homepage/homepage.dart';
 
@@ -11,14 +12,24 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => GithubProfileBloc()),
+        BlocProvider(create: (context) => ThemeModeCubit()),
       ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: MyHomePage(),
+      child: BlocConsumer<ThemeModeCubit, bool>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.pinkAccent,
+                brightness: state ? Brightness.light : Brightness.dark,
+              ),
+              useMaterial3: true,
+            ),
+            home: MyHomePage(),
+          );
+        },
       ),
     );
   }
