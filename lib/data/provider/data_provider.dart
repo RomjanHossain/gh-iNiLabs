d1989338d3a5ca8ecb0f5bf3f3bea26fdab8960d
RomjanHossain <romjanhossain726526@gmail.com>
2023-06-17 07:44:54 +0600

    repo provider ^

--- lib/data/provider/data_provider.dart
+++ lib/data/provider/data_provider.dart
@@ -2,6 +2,7 @@
 
 import 'package:gh/data/repo/profile_fetch_repo.dart';
 import 'package:gh/models/github_profile_mode.dart';
+import 'package:gh/models/github_repo_model.dart';
 import 'package:gh/utils/status_.dart';
 
 class DatabaseProvider {
@@ -9,5 +10,11 @@ class DatabaseProvider {
   Future<Status<GithubProfileModel, Exception>> getGithubProfile(
       String username) async {
     return await GetGithubProfileRepo(username).fetchProfile();
+  }
+
+  // get the repos
+  Future<Status<List<GithutRepoModel>, Exception>> getGithubRepos(
+      String username) async {
+    return await GetGithubProfileRepo(username).fetchRepos();
   }
 }
